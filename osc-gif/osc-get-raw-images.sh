fa24d6e5cd7cddbaf52a29f19a2378e57ec37a3f
siliconninja <siliconninja@users.noreply.github.com>
2020-02-28 19:35:01 -0500

    make it download full images, not thumbnails (creates duplicates though for some reason)

--- osc-gif/osc-get-raw-images.sh
+++ osc-gif/osc-get-raw-images.sh
@@ -2,5 +2,5 @@
 url="https://openstreetcam.org/details"
 header="access_token=0b6cf293c5f031815293f3fa19ebe75834c4f4b719b57975caed05d4297b876c&id=$1"
 curl $url --data $header -o osc_$1.json
-cat osc_$1.json | jq ".osv.photos[].th_name" | xargs -I{} echo "www.openstreetcam.org/"{} >>urls_$1.txt 
+cat osc_$1.json | jq ".osv.photos[].lth_name" | xargs -I{} echo "www.openstreetcam.org/"{} >>urls_$1.txt 
 cat urls_$1.txt | xargs -n 1 -P 100 wget
