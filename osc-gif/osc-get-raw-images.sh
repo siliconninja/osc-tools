0d028e76506e0788f1ec51ab005eb7b30dbe64ce
siliconninja <siliconninja@users.noreply.github.com>
2020-02-28 19:37:03 -0500

    Update osc-get-raw-images.sh

--- osc-gif/osc-get-raw-images.sh
+++ osc-gif/osc-get-raw-images.sh
@@ -4,3 +4,5 @@ header="access_token=0b6cf293c5f031815293f3fa19ebe75834c4f4b719b57975caed05d4297
 curl $url --data $header -o osc_$1.json
 cat osc_$1.json | jq ".osv.photos[].lth_name" | xargs -I{} echo "www.openstreetcam.org/"{} >>urls_$1.txt 
 cat urls_$1.txt | xargs -n 1 -P 100 wget
+# to avoid redownloading the same image URLs
+mv urls_$1.txt urls_$1_old.txt
