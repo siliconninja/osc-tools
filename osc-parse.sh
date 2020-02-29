2b4c93e24b1e62307829942203ad26b064d0fc32
siliconninja <siliconninja@users.noreply.github.com>
2020-02-28 19:03:17 -0500

    Fix curly brace issue that broke JSON in osc-parse
    Fixes the following error (extra } made JSON incorrect):
    
    Error: Invalid JSON (Unexpected RIGHT_BRACE("}") in state VALUE)
            at Parser.proto.parseError (/usr/lib/node_modules/json2csv/node_modules/jsonparse/jsonparse.js:321:16)
            at Parser.proto.onToken (/usr/lib/node_modules/json2csv/node_modules/jsonparse/jsonparse.js:373:21)
            at Parser.parser.onToken (/usr/lib/node_modules/json2csv/lib/JSON2CSVTransform.js:114:24)
            at Parser.proto.write (/usr/lib/node_modules/json2csv/node_modules/jsonparse/jsonparse.js:135:34)
            at JSON2CSVTransform._transform (/usr/lib/node_modules/json2csv/lib/JSON2CSVTransform.js:156:17)
            at JSON2CSVTransform.Transform._read (_stream_transform.js:191:10)
            at JSON2CSVTransform.Transform._write (_stream_transform.js:179:12)
            at doWrite (_stream_writable.js:454:12)
            at writeOrBuffer (_stream_writable.js:436:5)
            at JSON2CSVTransform.Writable.write (_stream_writable.js:327:11)

--- osc-parse.sh
+++ osc-parse.sh
@@ -3,4 +3,4 @@ osc_id=$1
 response=`curl -s "https://openstreetcam.org/details" --data "id=${osc_id}"`
 tail_num=`echo $response | jq .[] | grep -nw photos | awk -F\: '{print $1}'`
 echo $response | jq .[] | tail -n +$tail_num | sed 's/"photos"://g'  | ghead -n -1 | json2csv >$osc_id.csv
-echo $response | jq .[] | tail -n +7 | head -23 | sed  '$ s/.$/}}/' | json2csv -F >$osc_id-m.csv
+echo $response | jq .[] | tail -n +7 | head -23 | sed  '$ s/.$/}/' | json2csv -F >$osc_id-m.csv
